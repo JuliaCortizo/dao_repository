c57665c19b71e3a1d1ce8a4c0670b4a9560ca28a
jacg <jacg@gft.com>
2022-06-28 15:15:33 -0300

    Creating project and git repository

---
+++ src/db/DbIntegratyException.java
@@ -0,0 +1,10 @@
+package db;
+
+public class DbIntegratyException extends RuntimeException {
+	
+	public DbIntegratyException(String msg) {
+		super(msg);
+	}
+	private static final long serialVersionUID = 1L;
+
+}
