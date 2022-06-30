ba203a8048c0c70b049f035d547ed7ce469aaa19
jacg <jacg@gft.com>
2022-06-30 15:10:46 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
 		Department depart = new Department(1, "Books");
+		Seller seller = new Seller(1, "João Marcelo", "joao@gmail.com", new Date(), 2.400, depart);
 		System.out.println(depart);
+		System.out.println(seller);
 
 	}
 
